062a581fafadaf1fe923d30168e7d0a23da1e6c8
SamuelHalomoan <90833397+SamuelHalomoan@users.noreply.github.com>
2021-10-26 23:45:41 +0700

    Add files via upload

---
+++ view-latihan.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+    <title>latihan 1</title>
+</head>
+
+<body>
+    halo kawan.. yuk kita belajar web programming..!!!<br>
+    nilai 1 = <?= $nilai; ?>
+    nilai 2 = <?= $nilai; ?>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
+$nilai . " + " . $nilai2 . " = " . $hasil; ?>
+
+</body>
+
+</html> 
+
