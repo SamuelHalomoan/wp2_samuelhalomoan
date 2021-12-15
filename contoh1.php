a7e7f4cd4faecd1c1abd6ea4e5b1f68273e61fa0
SamuelHalomoan <90833397+SamuelHalomoan@users.noreply.github.com>
2021-12-15 16:45:17 +0700

    Add files via upload

--- contoh1.php
+++ contoh1.php
@@ -4,9 +4,8 @@ class contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>perkenalkan</h1>";
-        echo"nama saya imam namawi
-             saya tinggal di daerah ciputat
-             olahraga yang saya sukai adalah
-             bulutangkis";
+        echo"nama saya togu sitorus
+            saya tinggal di daerah ciputat 
+            olahraga yang saya suka bulutangkis";
     }
-}             
+}
